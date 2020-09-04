94c369c184ff9da52a9d9c09a11244b9681b1827
Yash Indane <yashindane46@gmail.com>
2020-09-04 22:30:39 +0530

    Update home.js

--- Digital Clock and Stopwatch/home.js
+++ Digital Clock and Stopwatch/home.js
@@ -19,8 +19,77 @@ var months = [
 
 function createAdjustBut(){
 
-  
   setTime_ = false;
+
+  var but = document.createElement('input');
+  but.setAttribute('class', 'p1');
+  but.setAttribute('value', '+');
+  
+
+  var but1 = document.createElement('input');
+  but1.setAttribute('class', 'p2');
+  but1.setAttribute('value', '+');
+  
+
+  var but2 = document.createElement('input');
+  but2.setAttribute('class', 'p3');
+  but2.setAttribute('value', '+');
+  
+
+  var but3 = document.createElement('input');
+  but3.setAttribute('class', 'p4');
+  but3.setAttribute('value', '-');
+  
+
+  var but4 = document.createElement('input');
+  but4.setAttribute('class', 'p5');
+  but4.setAttribute('value', '-');
+  
+
+  var but5 = document.createElement('input');
+  but5.setAttribute('class', 'p6');
+  but5.setAttribute('value', '-');
+  
+  var buts_array = [but , but1 , but2 , but3 , but4 , but5]
+  buts_array.forEach(y => y.setAttribute('type', 'button'));
+  buts_array.forEach(x => document.body.appendChild(x));
+  
+  but.onclick = function(){
+
+            hours = +hours;
+            hours += hours <= 22? 1 : -hours;
+            formatAndPlace();}
+
+  but3.onclick = function(){
+
+            hours = +hours;
+            hours -= hours >= 1 ? 1 : -23;
+            formatAndPlace();}  
+
+  but1.onclick = function(){
+
+            minutes = +minutes;
+            minutes += minutes <= 58? 1 : -minutes;
+            formatAndPlace();} 
+
+  but4.onclick = function(){
+
+            minutes = +minutes;
+            minutes -= minutes >= 1 ? 1 : -59;
+            formatAndPlace();} 
+
+  but2.onclick = function(){
+
+            secs = +secs;
+            secs += minutes <= 58? 1 : -secs;
+            formatAndPlace();}  
+
+  but5.onclick = function(){
+
+            secs = +secs;
+            secs -= secs >= 1 ? 1 : -59;
+            formatAndPlace();} 
+
 
   var but_d = document.createElement('input');
   but_d.setAttribute('type', 'button' );
@@ -28,59 +97,29 @@ function createAdjustBut(){
   but_d.setAttribute('value', 'Done');
   document.body.appendChild(but_d); 
 
-  var but = document.createElement('input');
-  but.setAttribute('type', 'button' );
-  but.setAttribute('class', 'p1');
-  but.setAttribute('value', '+');
-  document.body.appendChild(but); 
-
-  var but1 = document.createElement('input');
-  but1.setAttribute('type', 'button' );
-  but1.setAttribute('class', 'p2');
-  but1.setAttribute('value', '+');
-  document.body.appendChild(but1); 
-
-  var but2 = document.createElement('input');
-  but2.setAttribute('type', 'button' );
-  but2.setAttribute('class', 'p3');
-  but2.setAttribute('value', '+');
-  document.body.appendChild(but2); 
-
-  var but3 = document.createElement('input');
-  but3.setAttribute('type', 'button' );
-  but3.setAttribute('class', 'p4');
-  but3.setAttribute('value', '-');
-  document.body.appendChild(but3); 
-
-  var but4 = document.createElement('input');
-  but4.setAttribute('type', 'button' );
-  but4.setAttribute('class', 'p5');
-  but4.setAttribute('value', '-');
-  document.body.appendChild(but4); 
-
-  var but5 = document.createElement('input');
-  but5.setAttribute('type', 'button' );
-  but5.setAttribute('class', 'p6');
-  but5.setAttribute('value', '-');
-  document.body.appendChild(but5); 
+  but_d.onclick = function(){ 
+    setTime_ = true;
+    [but , but1 , but2 , but3 , but4 , but5 , but_d].forEach(b => b.remove());}     
+                              
+}  
 
 
 
+function formatAndPlace(){
 
+  [hours , minutes , secs] = ['' + hours , '' + minutes , '' + secs].map(
 
-}             
+  fld => fld.length == 1 ? '0' + fld : fld );
 
-
-function formatFields(tf) { return tf.length == 1 ? '0' + tf : tf ; }
+  timeField.innerHTML = `${hours} : ${minutes} : ${secs}`;}
 
 function runTime() {
 
-
-     
-        timeField.innerHTML = `${hours} : ${minutes} : ${secs}`;
         dateField.innerHTML =  date + ' ' + months[month] + ' ' + year;
 
         if (setTime_) { 
+
+          timeField.innerHTML = `${hours} : ${minutes} : ${secs}`;
 
           secs = +secs , minutes = +minutes , hours = +hours ;
         	
@@ -95,10 +134,14 @@ function runTime() {
             	hours = hours <= 22 ? hours + 1 : 0;
             }
 
-            [hours , minutes , secs] = ['' + hours , '' + minutes , '' + secs].map(formatFields);
+            [hours , minutes , secs] = ['' + hours , '' + minutes , '' + secs].map(
 
-        }  
+            fld => fld.length == 1 ? '0' + fld : fld );
 
+        } 
+
+        else { timeField.innerHTML = `${hours} : ${minutes} : ${secs}`; }
+          
 }	
 
 setInterval(runTime, 1000);
